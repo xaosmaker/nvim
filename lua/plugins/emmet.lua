7880c6b19caeb3dab50d9f5a7130e543408c2902
Drosos <drosinakis.drosos1@gmail.com>
2025-06-07 03:16:24 +0300

    initial nvim config

---
+++ lua/plugins/emmet.lua
@@ -0,0 +1,10 @@
+-- return {
+-- 	"olrtg/nvim-emmet",
+-- 	config = function()
+-- 		vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation)
+-- 	end,
+-- }
+return {
+	"mattn/emmet-vim",
+	lazy = false,
+}
