c79ab589022c2cb70d891b4a2b81a738419545d6
Drosos <drososD@proton.me>
2025-09-23 13:53:10 +0300

    main: remove the tailwindcss

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -26,14 +26,14 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-vim.keymap.set("n", "<C-up>", "<C-w><C-+>", { desc = "resize window height +" })
-vim.keymap.set("n", "<C-down>", "<C-w><C-->", { desc = "resize window height -" })
-vim.keymap.set("n", "<C-left>", "<C-w><C-<>", { desc = "resize window width -" })
-vim.keymap.set("n", "<C-right>", "<C-w><C->>", { desc = "resize window width +" })
+-- vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+-- vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+-- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+-- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- vim.keymap.set("n", "<C-up>", "<C-w><C-+>", { desc = "resize window height +" })
+-- vim.keymap.set("n", "<C-down>", "<C-w><C-->", { desc = "resize window height -" })
+-- vim.keymap.set("n", "<C-left>", "<C-w><C-<>", { desc = "resize window width -" })
+-- vim.keymap.set("n", "<C-right>", "<C-w><C->>", { desc = "resize window width +" })
 
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = " show diagnostics for line" })
 vim.keymap.set("n", "\\", "<cmd>NvimTreeToggle<CR>")
