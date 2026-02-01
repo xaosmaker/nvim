91bd1a98082ed0598eea06d317ae631b6884dde6
Drosinakis Drosos <drososD@proton.me>
2026-02-01 22:32:42 +0200

    add biome as formater in js,ts

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -36,10 +36,10 @@ return { -- Autoformat
 			python = { "isort", "black" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
-			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+			typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+			javascriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+			typescriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
 			nginx = { "nginxfmt" },
 			--C = { "clang-format" }, --"clangd",
 		},
