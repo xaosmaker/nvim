91eebc4a316fbe89c9734cf43ac8e6b1fb4489da
Drosinakis Drosos <drososD@proton.me>
2026-02-04 23:28:15 +0200

    chore: working config for vue_ls

---
+++ after/lsp/vtsls.lua
@@ -0,0 +1,47 @@
+---@module "vim.lsp.client"
+---@class vim.lsp.ClientConfig
+return {
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "vue",
+  },
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vim.fn.stdpath("data")
+                .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+            languages = { "vue" },
+            configNamespace = "typescript",
+          },
+        },
+      },
+    },
+    -- typescript = {
+    --   inlayHints = {
+    --     enumMemberValues = {
+    --       enabled = true,
+    --     },
+    --     functionLikeReturnTypes = {
+    --       enabled = true,
+    --     },
+    --     parameterNames = { enabled = "all" },
+    --     parameterTypes = {
+    --       enabled = true,
+    --       suppressWhenArgumentMatchesName = true,
+    --     },
+    --     propertyDeclarationTypes = {
+    --       enabled = true,
+    --     },
+    --     variableTypes = {
+    --       enabled = true,
+    --     },
+    --   },
+    -- },
+  },
+}
