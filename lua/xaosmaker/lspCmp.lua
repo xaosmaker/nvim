b758d0824acc0d3fad2c81021f96ccd681915a92
Drosos Drosinakis <drosinakis.drosos1@gmail.com>
2026-02-06 02:45:35 +0200

    working Good need formatters

--- lua/xaosmaker/lspCmp.lua
+++ lua/xaosmaker/lspCmp.lua
@@ -6,12 +6,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
       -- Create a keymap for vim.lsp.buf.implementation ...
     end
     -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-    if client:supports_method('textDocument/completion') then
-      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-      -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-      -- client.server_capabilities.completionProvider.triggerCharacters = chars
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-    end
+    -- if client:supports_method('textDocument/completion') then
+    -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+    -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+    -- client.server_capabilities.completionProvider.triggerCharacters = chars
+    -- vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    -- end
     -- Auto-format ("lint") on save.
     -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
     if not client:supports_method('textDocument/willSaveWaitUntil')
@@ -20,7 +20,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
         buffer = args.buf,
         callback = function()
-          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+          -- vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
         end,
       })
     end
