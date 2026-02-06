afdb9ec9051434ff7b4db437f15ff678b8bd1b38
Drosos Drosinakis <drosinakis.drosos1@gmail.com>
2026-02-06 07:18:32 +0200

    chore: add telescope keymaps

--- lua/xaosmaker/plugins/telescope.lua
+++ lua/xaosmaker/plugins/telescope.lua
@@ -1,42 +1,49 @@
 return { -- Fuzzy Finder (files, lsp, etc)
-  "nvim-telescope/telescope.nvim",
-  event = "VimEnter",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
+	"nvim-telescope/telescope.nvim",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
 
-      build = "make",
+			build = "make",
 
-      cond = function()
-        return vim.fn.executable("make") == 1
-      end,
-    },
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+	},
+	config = function()
+		local telescope = require("telescope")
+		local telescopt_builtin = require("telescope.builtin")
 
-  },
-  config = function()
-    local telescope = require("telescope")
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+			},
+		})
 
+		-- Enable Telescope extensions if they are installed
+		pcall(require("telescope").load_extension, "fzf")
+		pcall(require("telescope").load_extension, "ui-select")
 
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-      }
-    })
+		-- See `:help telescope.builtin`
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
-    -- Enable Telescope extensions if they are installed
-    pcall(require("telescope").load_extension, "fzf")
-    pcall(require("telescope").load_extension, "ui-select")
-
-    -- See `:help telescope.builtin`
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-    vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
-    vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-    vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-    vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-    vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-  end,
+		vim.keymap.set("n", "gd", telescopt_builtin.lsp_definitions, { desc = "[G]oto [D]efinition" })
+		vim.keymap.set("n", "gr", telescopt_builtin.lsp_references, { desc = "[G]oto [R]eferences" })
+		vim.keymap.set("n", "gI", telescopt_builtin.lsp_implementations, { desc = "[G]oto [I]mplementation" })
+		vim.keymap.set("n", "<leader>D", telescopt_builtin.lsp_type_definitions, { desc = "Type [D]efinition" })
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "[G]oto [D]eclaration" })
+	end,
 }
