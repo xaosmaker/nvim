f0cc98b338bba927536d32b8b581cefc47a1f68a
xaosmaker <drosinakis.drosos1@gmail.com>
2026-02-11 11:34:52 +0200

    chore: change to cmp completion

---
+++ lua/xaosmaker/plugins/lsp/cmp.lua
@@ -0,0 +1,46 @@
+return {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+	},
+	config = function()
+		local cmp = require("cmp")
+
+		cmp.setup({
+			-- Snippet engine (required)
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			-- Window appearance (optional)
+			window = {
+				-- completion = cmp.config.window.bordered(),
+				-- documentation = cmp.config.window.bordered(),
+			},
+
+			-- Key mappings (required)
+			mapping = cmp.mapping.preset.insert({
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<TAB>"] = cmp.mapping.select_next_item(),
+				["<S-TAB>"] = cmp.mapping.select_prev_item(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			}),
+
+			-- Completion sources (required, in order of priority)
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "cmdline" },
+				{ name = "path" },
+			}),
+		})
+	end,
+}
--- lua/xaosmaker/plugins/lsp/mason_cmp.lua
+++ lua/xaosmaker/plugins/lsp/mason_cmp.lua
@@ -1,6 +1,5 @@
 return {
 	{
-
 		"mason-org/mason-lspconfig.nvim",
 		opts = {
 			ensure_installed = {
