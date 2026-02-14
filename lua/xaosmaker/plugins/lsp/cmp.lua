8894ec5d5f81dc49aeb10978e393ee85e2ef4e23
xaosmaker <drosinakis.drosos1@gmail.com>
2026-02-14 19:16:20 +0200

    chore: add more snippets

--- lua/xaosmaker/plugins/lsp/cmp.lua
+++ lua/xaosmaker/plugins/lsp/cmp.lua
@@ -8,6 +8,12 @@ return {
 		"hrsh7th/cmp-cmdline",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
+		{
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		},
 	},
 	config = function()
 		local cmp = require("cmp")
