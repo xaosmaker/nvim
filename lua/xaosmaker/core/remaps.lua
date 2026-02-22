1ae1ac1d5f696dbadc5953e2084e55ba54713412
xaosmaker <drosinakis.drosos1@gmail.com>
2026-02-22 03:32:30 +0200

    feat: add oil.lua with git signs

--- lua/xaosmaker/core/remaps.lua
+++ lua/xaosmaker/core/remaps.lua
@@ -3,5 +3,4 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = " show diagnostics for line" })
-vim.keymap.set("n", "\\", "<cmd>Ex<CR>")
-
+vim.keymap.set("n", "\\", "<cmd>Oil<CR>")
