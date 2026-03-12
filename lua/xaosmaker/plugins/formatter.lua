45df0eecd998534ffffe34be24cae73ef0c5fc8b
xaosmaker <drosinakis.drosos1@gmail.com>
2026-03-12 19:12:26 +0200

    add detect if biome or eslint

--- lua/xaosmaker/plugins/formatter.lua
+++ lua/xaosmaker/plugins/formatter.lua
@@ -2,26 +2,26 @@ return {
 	"stevearc/conform.nvim",
 
 	config = function()
-		local isBiome = require("xaosmaker.utils").file_exist("biome.js")
-		local formatter = "prettierd"
-		if isBiome then
-			formatter = "biome"
+		local isBiome = require("xaosmaker.utils").file_exist("biome.json")
+		local formatter = { "prettierd" }
+		if isBiome > 0 then
+			formatter = { "biome", "biome-organize-imports" }
 		end
 
 		require("conform").formatters.prettierd = {
-			inherit,
+			inherit = true,
 		}
 		require("conform").setup({
 
 			formatters_by_ft = {
 
 				lua = { "stylua" },
-				javascript = { formatter, stop_after_first = true },
-				javascriptreact = { formatter, stop_after_first = true },
-				typescriptreact = { formatter, stop_after_first = true },
-				typescript = { formatter, stop_after_first = true },
+				javascript = formatter,
+				javascriptreact = formatter,
+				typescriptreact = formatter,
+				typescript = formatter,
 				go = { "gopls" },
-				vue = { formatter, stop_after_first = true },
+				vue = formatter,
 			},
 			format_on_save = {
 				timeout_ms = 500,
