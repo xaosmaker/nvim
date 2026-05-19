575a8ae8013ce4a0d3f671ead36f0e48dbbb24af
xaosmaker <drosinakis.drosos1@gmail.com>
2026-05-19 22:56:24 +0300

    remove omni had problems

--- lua/xaosmaker/plugins/lsp/completion.lua
+++ lua/xaosmaker/plugins/lsp/completion.lua
@@ -47,7 +47,7 @@ return {
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { "lsp", "dadbod", "path", "buffer", "snippets", "omni" },
+			default = { "lsp", "dadbod", "path", "buffer", "snippets" },
 			-- per_filetype = {
 			-- 	sql = { "lsp", "dadbod", "buffer" },
 			-- 	-- optionally inherit from the `default` sources
